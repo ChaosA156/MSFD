a93be5fb88e1f056abaa110c3155e07b18916a6a
ChaosA156 <ChaosA156@gmail.com>
2024-01-24 13:19:06 -0600

    Colors

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+
+    public static MainManager Instance;
+
+    public Color TeamColor;
+
+    private void Awake()
+    {
+       if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
